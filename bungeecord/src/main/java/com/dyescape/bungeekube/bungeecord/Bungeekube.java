1869804957bd60cdd5734643fe4cb012589331ec
Dennis van der Veeke <mrdienns@dyescape.com>
2020-11-27 19:59:54 +0100

    now automatically registering discovered service

--- bungeecord/src/main/java/com/dyescape/bungeekube/bungeecord/Bungeekube.java
+++ bungeecord/src/main/java/com/dyescape/bungeekube/bungeecord/Bungeekube.java
@@ -6,8 +6,10 @@ import com.dyescape.bungeekube.kubernetes.discovery.ServiceDiscovery;
 
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.plugin.Plugin;
 
+import java.net.InetSocketAddress;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -24,12 +26,22 @@ public class Bungeekube extends Plugin {
 
         if (!foundServices.isEmpty()) {
             LOGGER.info("Discovered these services:");
+        } else {
+            LOGGER.warning("No services found! Did you follow the installation manual?");
         }
 
-        for (DiscoveredService discoveredService : discovery.Discover()) {
-            LOGGER.info(String.format("  - %s:%s (%s)", discoveredService.getHost(), discoveredService.getPort(),
-                    discoveredService.getName()));
+        for (DiscoveredService service : discovery.Discover()) {
+            LOGGER.info(String.format("  - %s:%s (%s)", service.getHost(), service.getPort(),
+                    service.getName()));
+
+            this.registerServer(service);
         }
+    }
+
+    private void registerServer(DiscoveredService service) {
+        ServerInfo info = this.getProxy().constructServerInfo(service.getName(),
+                new InetSocketAddress(service.getHost(), service.getPort()), "", false);
+        this.getProxy().getServers().put(service.getName(), info);
     }
 
     private KubernetesClient createKubernetesClient() {
